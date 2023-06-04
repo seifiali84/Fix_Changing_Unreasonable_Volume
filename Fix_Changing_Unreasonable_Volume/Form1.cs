dd632eca6c34509d9ed0de0705a266506313e3ba
Ali Seifi <seifiali84@gmail.com>
2023-06-04 22:51:55 +0330

    Add project files.

---
+++ Fix_Changing_Unreasonable_Volume/Form1.cs
@@ -0,0 +1,10 @@
+namespace Fix_Changing_Unreasonable_Volume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
