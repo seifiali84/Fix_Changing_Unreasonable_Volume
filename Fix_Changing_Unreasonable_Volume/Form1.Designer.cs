6dededdf91c06f6cdc2573611044b551d2e3d19c
Ali Seifi <seifiali84@gmail.com>
2023-06-04 23:15:00 +0330

    creating a simple program as v1.0.0

--- Fix_Changing_Unreasonable_Volume/Form1.Designer.cs
+++ Fix_Changing_Unreasonable_Volume/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Lock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "set mic volume to 100%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(257, 248);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private Button button1;
+        private Label label1;
     }
 }
--- Fix_Changing_Unreasonable_Volume/Form1.cs
+++ Fix_Changing_Unreasonable_Volume/Form1.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using NAudio.CoreAudioApi;
+
 namespace Fix_Changing_Unreasonable_Volume
 {
     public partial class Form1 : Form
@@ -5,6 +8,25 @@ namespace Fix_Changing_Unreasonable_Volume
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static void AudioEndpointVolume_OnVolumeNotification(AudioVolumeNotificationData data)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            MMDevice speakers = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia); // Get default multimedia playback device
+
+            speakers.AudioEndpointVolume.MasterVolumeLevelScalar = 1.0f; // Set speaker volume level to maximum (1.0f)
+
+            Console.WriteLine("Microphone volume changed - Speakers set to Max Volume!");
+            Console.Beep();   // Play sound notification when speaker volumes are set at max.
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator(); // Create a new instance of the audio device enumerator 
+            MMDevice mic = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications); // Get the default communication microphone
+
+            mic.AudioEndpointVolume.OnVolumeNotification += AudioEndpointVolume_OnVolumeNotification; // Subscribe to Volume Notification events
         }
     }
 }
